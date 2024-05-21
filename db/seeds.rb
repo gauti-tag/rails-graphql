e66b7434777aa29e89b16497ab22576649279c24
Gautier Tiehoule <gautier.tiehoule@gmail.com>
2024-05-21 20:28:15 +0000

    Feat: Populate the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+2.times do
+   author = Author.create(name: Faker::Name.name , email: Faker::Internet.email)
+   3.times do
+        book = author.books.create(name: Faker::Lorem.sentence(word_count: 2) , decription: Faker::Lorem.paragraph(sentence_count: 2))
+        2.times do
+            book.chapters.create(name: Faker::Lorem.sentence(word_count: 2) , short_description: Faker::Lorem.paragraph(sentence_count: 2))
+        end
+   end
+end
